d10b5ce66f1b428f1faf05e960ee4e6d0bb6310f
Duong <duongkhanhbangcb2021@gmail.com>
2026-05-18 18:55:43 +0700

    Thêm chương trình Hello World và giới thiệu bản thân

--- weeks/week-01-hello-python/exercises/ex01_hello.py
+++ weeks/week-01-hello-python/exercises/ex01_hello.py
@@ -8,38 +8,37 @@ Chạy file này bằng: python ex01_hello.py
 """
 
 # TODO 1: In ra dòng chữ "Hello, World!"
-# Viết code ở đây:
+
+print("Hello, World!")
 
 
 # TODO 2: In ra tên của bạn trên một dòng riêng
-# Viết code ở đây:
+
+print("Dương Khánh Băng")
 
 
 # TODO 3: In ra 3 điều bạn thích, mỗi điều trên một dòng
-# Ví dụ:
-# Tôi thích ăn phở
-# Tôi thích nghe nhạc
-# Tôi thích code Python
-# Viết code ở đây:
+
+print("Tôi thích ăn phở")
+print("Tôi thích nghe nhạc")
+print("Tôi thích code Python")
 
 
 # TODO 4: In ra một hình chữ nhật bằng dấu *
-# Kết quả mong muốn:
-# *****
-# *   *
-# *   *
-# *****
-# Viết code ở đây:
+
+print("*****")
+print("*   *")
+print("*   *")
+print("*****")
 
 
 # TODO 5 (Thử thách): In ra tên bạn bằng ký tự đặc biệt
-# Ví dụ nếu tên là "HI":
-# H   H  III
-# H   H   I
-# HHHHH   I
-# H   H   I
-# H   H  III
-# Viết code ở đây:
+
+print("DDDDD  K   K  BBBB")
+print("D   D  K  K   B   B")
+print("D   D  KKK    BBBB")
+print("D   D  K  K   B   B")
+print("DDDDD  K   K  BBBB")
 
 
 print("\n🎉 Chúc mừng! Bạn đã hoàn thành bài tập đầu tiên!")
