118b15a735298e1fa622328b9b33da543d39c11a
Duong <duongkhanhbangcb2021@gmail.com>
2026-05-18 19:43:36 +0700

    TODO 1: Thêm biến ten Thêm biến tuoi Thêm biến diem_tb Thêm biến dang… …_hoc Thêm print() và type() TODO 2: Thêm biến a, b Thêm hoán đổi a, b = b, a Thêm in kết quả trước/sau hoán đổi TODO 3: Thêm phép += Thêm phép -= Thêm phép *= Thêm phép //= Thêm print() sau mỗi bước TODO 4: Thêm multiple assignment ho, ten, tuoi = ... Thêm f-string in họ tên và tuổi

--- weeks/week-02-variables-types/exercises/ex01_variables.py
+++ weeks/week-02-variables-types/exercises/ex01_variables.py
@@ -5,25 +5,56 @@ Mục tiêu: Hiểu cách khai báo và sử dụng biến
 """
 
 # TODO 1: Tạo 4 biến lưu thông tin cá nhân
-# ten = ???       (str)
-# tuoi = ???      (int)
-# diem_tb = ???   (float)
-# dang_hoc = ???  (bool)
+
+ten = "Dương Khánh Băng"   # str
+tuoi = 20                  # int
+diem_tb = 8.5              # float
+dang_hoc = True            # bool
+
 # In ra giá trị và kiểu dữ liệu của mỗi biến bằng type()
+
+print(ten, type(ten))
+print(tuoi, type(tuoi))
+print(diem_tb, type(diem_tb))
+print(dang_hoc, type(dang_hoc))
 
 
 # TODO 2: Hoán đổi giá trị 2 biến KHÔNG dùng biến tạm
-# a = 10
-# b = 20
-# Sau hoán đổi: a = 20, b = 10
-# Gợi ý: Python cho phép a, b = b, a
+
+a = 10
+b = 20
+
+print("Trước hoán đổi:")
+print("a =", a)
+print("b =", b)
+
+# Hoán đổi
+a, b = b, a
+
+print("Sau hoán đổi:")
+print("a =", a)
+print("b =", b)
 
 
 # TODO 3: Augmented assignment
-# Cho x = 100. Dùng +=, -=, *=, //= để biến đổi x qua 4 bước
-# In ra x sau mỗi bước
+
+x = 100
+
+x += 50
+print("Sau += :", x)
+
+x -= 20
+print("Sau -= :", x)
+
+x *= 2
+print("Sau *= :", x)
+
+x //= 5
+print("Sau //= :", x)
 
 
 # TODO 4 (Thử thách): Multiple assignment
-# Gán 3 biến trên 1 dòng: ho, ten, tuoi = ???
-# In ra: "Họ tên: [ho] [ten], [tuoi] tuổi"
+
+ho, ten, tuoi = "Dương", "Khánh Băng", 20
+
+print(f"Họ tên: {ho} {ten}, {tuoi} tuổi")
