72fa1a3516428fad970889cf56a03f7079562875
Duong <duongkhanhbangcb2021@gmail.com>
2026-05-18 18:59:57 +0700

    Hoàn thành mini project ASCII Art Generator

--- weeks/week-01-hello-python/mini-project/starter.py
+++ weeks/week-01-hello-python/mini-project/starter.py
@@ -7,18 +7,23 @@ Chạy: python starter.py
 """
 
 # Bước 1: Hỏi tên người dùng
+
 ten = input("Nhập tên của bạn: ")
 
 # Bước 2: Tính độ rộng khung
-# TODO: Tính width dựa trên len(ten)
+
+width = len(ten) + 6
 
 # Bước 3: In khung trên
-# TODO: In dòng trên bằng ╔═══╗
+
+print("╔" + "═" * width + "╗")
 
 # Bước 4: In nội dung
-# TODO: In tên trong khung, căn giữa
+
+noi_dung = f"  {ten}  "
+
+print("║" + noi_dung.center(width) + "║")
 
 # Bước 5: In khung dưới
-# TODO: In dòng dưới bằng ╚═══╝
 
-# Gợi ý: Dùng str.center(width) để căn giữa
+print("╚" + "═" * width + "╝")
